8c356010b107699d68d05c6b5b54875a89113a63
Kenny Neutron <89734947+KennyNeutron@users.noreply.github.com>
2024-11-22 22:16:54 +0800

    Update Count_OneDigit.ino

--- examples/Count_OneDigit/Count_OneDigit.ino
+++ examples/Count_OneDigit/Count_OneDigit.ino
@@ -1,15 +1,19 @@
 #include "SevenSeg595.h"
 
-SevenSeg595 ss;
+SevenSeg595 ssCA;
 
+uint8_t counter = 0;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  ssCA.begin(8,9,10,11, false);
 }
 
 void loop() {
-  digitalWrite(ledPin, LOW);
+  ssCA.OneDigit(counter, false);
+  ssCA.UpdateDisplay();
   delay(1000);
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
+  counter++;
+  if(counter>=10){
+    counter=0;
+  }
 }
